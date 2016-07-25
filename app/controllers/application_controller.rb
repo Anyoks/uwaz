23f70e46bbb1fbbf791977a05553397a0354f6af
Denn Orina <anyoksdenn@gmail.com>
2016-07-25 17:48:15 +0300

    Updated  Devise users to the new version and updated the corresponding code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
     protected
 
       def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password) }
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password]) 
+          devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password])
       end
 
     private
